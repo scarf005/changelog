7c3e2c7608de8365e88988d0d8f70ece1aa554b7
scarf <greenscarf005@gmail.com>
2022-12-22 03:28:16 +0900

    fix: use this

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -3,37 +3,42 @@ import java.util.*
 
 @Serializable
 data class Template(
-    val changelog: String, val section: String, val items: String
+    val changelog: String,
+    val section: String,
+    val items: String,
+    val replace: Map<String, String>? = null
 ) {
+// TODO: assert only allowed {key}s are in template string
+
     private fun ConventionalCommit.renderItem() = items.replace("{desc}", desc)
     private fun SortedMap<ChangelogSectionType, List<ConventionalCommit>>.renderSections() =
         map { (key, commits) ->
             val list = commits.joinToString("\n") { it.renderItem() }
             section
                 .replace("{desc}", key.desc)
-                .replace("{items}", list) + "\n"
+                .replace("{items}", list)
         }
 
     fun toChangelog(
         commits: List<ConventionalCommit>,
-        section: ChangelogSections,
+        changelogSections: ChangelogSections,
         criteria: VersionCriteria
     ): String {
         val date = commits.first().commit.date()
         val version = commits.version(criteria)
 
         return commits
-            .toSections(section)
+            .toSections(changelogSections)
             .map { (key, commits) ->
                 val list = commits.joinToString("\n") {
                     it.renderItem()
                 }
-                markdownTemplate.section
+                section
                     .replace("{desc}", key.desc)
-                    .replace("{items}", list) + "\n"
+                    .replace("{items}", list)
             }
             .let {
-                markdownTemplate.changelog
+                changelog
                     .replace("{sections}", it.joinToString("\n"))
                     .replace("{tag}", version.toString())
                     .replace("{date}", date)

036dbccd2bb26bbbe3999f4396fa6a3fdf03ebc9
scarf <greenscarf005@gmail.com>
2022-12-22 22:13:59 +0900

    test: migrate to new API

--- src/test/kotlin/CommitWithCode.kt
+++ src/test/kotlin/CommitWithCode.kt
@@ -1,16 +1,23 @@
-import changelog.Changelog
-import changelog.toBBCode
+import changelog.Template
+import com.github.syari.kgit.KGit
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.io.File
 import kotlin.io.path.Path
 import kotlin.io.path.div
 import kotlin.io.path.readText
 
 object CommitWithCode : StringSpec({
-    val changelog = Changelog(begin = "ec0b149", end = "ec0b149")
+    val commit = KGit.open(File(".")).run {
+        repository.parseCommit(repository.resolve("ec0b149"))
+    }.let(ConventionalCommit.Companion::of)
+
+
     val resources = Path("src/test/resources") / "withCode.bbcode"
 
     "ec0b149" {
-        changelog.toBBCode() shouldBe resources.readText()
+        Template.builder(
+            listOf(commit!!),
+        )(templates["bbcode"]!!) shouldBe resources.readText()
     }
 })

b64528eb2e146d8196d86153fd86cb457fc143cd
scarf <greenscarf005@gmail.com>
2023-01-15 16:00:10 +0900

    feat: allow counting version from previous version
    for compatibility, new versions will be counted from

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ import group.CommitGroup
 import group.ConventionalCommit
 import kotlinx.serialization.decodeFromString
 import mu.KotlinLogging
+import org.eclipse.jgit.lib.Ref
 import org.eclipse.jgit.revwalk.RevCommit
 import version.SemVer
 import version.VersionCriteria
@@ -13,6 +14,7 @@ import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.div
 import kotlin.io.path.readText
+import kotlin.io.path.writeText
 
 private val logger = KotlinLogging.logger {}
 
@@ -27,8 +29,8 @@ fun RevCommit.isConventional() = conventionalTitle.matches(shortMessage)
 fun RevCommit.parent(): RevCommit = parents.singleOrNull() ?: this
 
 /** @receiver commits from latest to oldest */
-fun List<ConventionalCommit>.version(criteria: VersionCriteria) = asReversed()
-    .fold(SemVer()) { acc, commit ->
+fun List<ConventionalCommit>.version(begin: SemVer, criteria: VersionCriteria) = asReversed()
+    .fold(begin) { acc, commit ->
         val version = commit.parseVersion(criteria)
         if (version != null) logger.debug { "$acc + $version -> ${acc + version} :: $commit" }
         acc + version
@@ -39,18 +41,34 @@ fun Path.toConfig() = Yaml.default.decodeFromString<Config>(readText())
 val resources = Path("src/main/resources")
 val markdown = (resources / "markdown.yaml").toConfig()
 val bbcode = (resources / "bbcode.yaml").toConfig()
+val sts = (resources / "slaythespire.yaml").toConfig()
+
+val manual = Path("/home/scarf/repo/Marisa/docs/changelog")
+
+fun Ref.toSemVer() = name.substringAfterLast('v').split(".").map { it.toInt() }
+    .let { (major, minor, patch) -> SemVer(major, minor, patch) }
 
 fun main() {
     KGit.open(cwd.toFile()).run {
-        val begin = prevId(findId("v0.0.0"))
-        val commits = log { addRange(begin, HEAD()) }
+        val lastTag = tagList().last()
+        val commits = log { addRange(lastTag.objectId, HEAD()) }
             .mapNotNull { ConventionalCommit.of(it) }
 
-        ChangelogGenerator(CommitGroup(commits)).render(bbcode).also(::println)
+        val group = CommitGroup(lastTag.toSemVer(), commits)
+        val changelog = ChangelogGenerator(group)
 
+        println(changelog.render(markdown))
+
+        mapOf(markdown to "md", bbcode to "bbcode", sts to "sts.txt")
+            .map { (k, ext) -> changelog.render(k) to (manual / "changelog.$ext") }
+            .forEach { (text, file) -> file.writeText(text) }
+
+        (manual / "version.txt").writeText(group.version.toString())
+//        if (lastTag.toSemVer() == group.version) return
+//
 //        tag {
-//            name = changelog.tag
-//            message = changelog.toMarkdown()
+//            name = group.version.tag
+//            message = changelog.render(markdown)
 //        }
     }
 }
